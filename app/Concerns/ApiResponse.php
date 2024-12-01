184b19732d93ca81a5092969047f7742e8a32e63
holiq <holiq.ibrahim376@gmail.com>
2024-12-01 19:34:03 +0700

    feat(concern): support collection for success response

--- app/Concerns/ApiResponse.php
+++ app/Concerns/ApiResponse.php
@@ -4,13 +4,14 @@ namespace App\Concerns;
 
 use App\Enums\HttpStatus;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Collection;
 
 trait ApiResponse
 {
     /**
-     * @param  array<string, mixed>  $data
+     * @param  array<string, mixed> | Collection<string, mixed>  $data
      */
-    public function resolveSuccessResponse(string $message, array $data = [], HttpStatus | int $status = HttpStatus::OK): JsonResponse
+    public function resolveSuccessResponse(string $message, array | Collection $data = [], HttpStatus | int $status = HttpStatus::OK): JsonResponse
     {
         $status = is_int($status) ? $status : $status->value;
 
@@ -20,7 +21,7 @@ trait ApiResponse
                 'message' => $message,
                 'data' => $data,
             ],
-            status: $status
+            status: $status,
         );
     }
 
@@ -37,7 +38,7 @@ trait ApiResponse
                 'message' => $message,
                 'errors' => $errors,
             ],
-            status: $status
+            status: $status,
         );
     }
 }
