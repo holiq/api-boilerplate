a65e0231e09d509b192eeaba164a503a66dd4ee2
holiq <holiq.ibrahim376@gmail.com>
2024-12-01 19:34:36 +0700

    feat(test): add test login after account registered

--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -16,6 +16,26 @@ it('can login', function () {
     $response->assertOk();
 });
 
+it('can login from registered account', function () {
+    $response = $this->post(route(name: 'register'), [
+        'name' => 'John Doe',
+        'email' => 'john@doe.com',
+        'password' => 'password',
+        'password_confirmation' => 'password',
+    ]);
+
+    $response->assertCreated();
+
+    expect($response->content())->toContain(needles: 'John Doe');
+
+    $response = $this->post(route(name: 'login'), [
+        'email' => 'john@doe.com',
+        'password' => 'password',
+    ]);
+
+    $response->assertOk();
+});
+
 it('can not login if credentials are invalid', function () {
     $response = $this->post(route(name: 'login'), [
         'email' => 'invalid@mail.com',
